098693d67e3aeefeae09e46355389d15d268a94f
Unknown <miguel.valerio@tecnico.ulisboa.pt>
2018-04-19 15:55:03 +0100

    ja le e guarda com a representacao que combinamos
    yoooooooooo

--- proj.c
+++ proj.c
@@ -2,9 +2,8 @@
 #include <stdlib.h>
 
 int main(int argc, char const *argv[]) {
-  int m, n, i, j;
-  int **peso_plano, **peso_cenario;
-  int **pes_lig_hor, **pes_lig_ver;
+  int m, n, i, j, temp;
+  int **grafo_cap;
 
   scanf("%d %d\n\n", &m, &n);
   if (m < 1 || n < 1) {
@@ -12,70 +11,81 @@ int main(int argc, char const *argv[]) {
     return 1;
   }
 
-  peso_plano = (int**) malloc(m * sizeof(int*));
-  for (i = 0; i < m; i++) {
-    peso_plano[i] = (int*) malloc(n * sizeof(int));
-    for (j = 0; j < n; j++) {
-      scanf("%d", &peso_plano[i][j]);
+  // Cria a matriz onde estarao as capacidades das ligacoes
+  grafo_cap = (int**) malloc((m*n + 2)*sizeof(int*));
+  for (i = 0; i < (m*n + 2); i++) {
+    grafo_cap[i] = (int*) malloc((m*n + 2)*sizeof(int));
+    for (j = 0; j < (m*n)+2; j++) {
+      grafo_cap[i][j] = 0;
     }
   }
 
-  peso_cenario = (int**) malloc(m * sizeof(int*));
-  for (i = 0; i < m; i++) {
-    peso_cenario[i] = (int*) malloc(n * sizeof(int));
-    for (j = 0; j < n; j++) {
-      scanf("%d", &peso_cenario[i][j]);
-    }
+  // Le e guarda os pesos de plano --> capacidades de ligar ao vertice P (i=0)
+  for (i = 1; i < (m*n)+1; i++) {
+    scanf("%d", &grafo_cap[0][i]);
   }
 
-  pes_lig_hor = (int**) malloc(m * sizeof(int*));
-  for (i = 0; i < m; i++) {
-    pes_lig_hor[i] = (int*) malloc((n-1) * sizeof(int));
-    for (j = 0; j < n-1; j++) {
-      scanf("%d", &pes_lig_hor[i][j]);
-    }
+  // Le e guarda os pesos de cenario --> capacidades de ligar ao vertice C (i=(m*n)+1)
+  for (i = 1; i < (m*n)+1; i++) {
+    scanf("%d", &grafo_cap[(m*n)+1][i]);
   }
 
-  pes_lig_ver = (int**) malloc((m-1) * sizeof(int*));
-  for (i = 0; i < m-1; i++) {
-    pes_lig_ver[i] = (int*) malloc(n * sizeof(int));
-    for (j = 0; j < n; j++) {
-      scanf("%d", &pes_lig_ver[i][j]);
-    }
+  // Le e guarda as capacidades das ligacoes horizontais
+  for (i = 1; i < (m*(n-1))+1; i++) {
+    scanf("%d", &grafo_cap[i][i+1]);
   }
 
+  // Le e guarda as capacidades das ligacoes verticais
+  for (i = 1; i < ((m-1)*n)+1; i++) {
+    scanf("%d", &grafo_cap[i][i+n]);
+  }
 
+  /* Consideramos que cada vertice (pixel) so liga ao seu vizinho da esquerda
+      e ao vizinho de baixo.
+     Todos os vertices ligam ao vertice P e C */
+
+  temp = 0;
   printf("%d %d\n\n", m, n);
-
-  for (i = 0; i < m; i++) {
-    for (j = 0; j < n; j++) {
-      printf("%d ", peso_plano[i][j]);
+  for (i = 1; i < (m*n + 1); i++) {
+    printf("%d ", grafo_cap[0][i]);
+    temp++;
+    if (temp == n) {
+      printf("\n");
+      temp = 0;
     }
-    printf("\n");
   }
   printf("\n");
 
-  for (i = 0; i < m; i++) {
-    for (j = 0; j < n; j++) {
-      printf("%d ", peso_cenario[i][j]);
+  temp = 0;
+  for (i = 1; i < (m*n + 1); i++) {
+    printf("%d ", grafo_cap[(m*n)+1][i]);
+    temp++;
+    if (temp == n) {
+      printf("\n");
+      temp = 0;
     }
-    printf("\n");
   }
   printf("\n");
 
-  for (i = 0; i < m; i++) {
-    for (j = 0; j < n-1; j++) {
-      printf("%d ", pes_lig_hor[i][j]);
+  temp = 0;
+  for (i = 1; i < (m*(n-1))+1; i++) {
+    printf("%d ", grafo_cap[i][i+1]);
+    temp++;
+    if (temp == (n-1)) {
+      printf("\n");
+      temp = 0;
     }
-    printf("\n");
   }
   printf("\n");
 
-  for (i = 0; i < m-1; i++) {
-    for (j = 0; j < n; j++) {
-      printf("%d ", pes_lig_ver[i][j]);
+  temp = 0;
+  for (i = 1; i < ((m-1)*n)+1; i++) {
+    printf("%d ", grafo_cap[i][i+n]);
+    temp++;
+    if (temp == n) {
+      printf("\n");
+      temp = 0;
     }
-    printf("\n");
   }
   printf("\n");
 
