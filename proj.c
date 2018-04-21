df608dc543b4242e53f3f2961f91a0e69475ce3b
Unknown <miguel.valerio@tecnico.ulisboa.pt>
2018-04-21 20:19:49 +0100

    A BFS ja funciona e devolve o pai do target
    ou entao -1 se nao houver caminho

--- proj.c
+++ proj.c
@@ -1,4 +1,4 @@
- #include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 
@@ -10,6 +10,10 @@
 #define BAIXO 3
 #define ESQ 4
 #define C 5
+
+#define BRANCO 0
+#define CINZA 1
+#define PRETO 2
 // ----------------------------------------------------------------------------
 
 
@@ -18,7 +22,7 @@
 typedef struct no {
   int id;
   struct no *next;
-} Lista;
+} No;
 // ----------------------------------------------------------------------------
 
 
@@ -26,8 +30,15 @@ typedef struct no {
 
 // ---------------------------- Variaveis Globais ----------------------------
 int n_colunas, n_linhas;
+int n_vertices; // --> ja conta com o vertice P (source)
 int **grafo_cap;
 int *peso_plano, *peso_cenario;
+
+int *res;
+
+int fluxoMin;
+int *cor, *dist, *pai;
+No  *lista_incio, *lista_fim;
 // ----------------------------------------------------------------------------
 
 
@@ -40,29 +51,142 @@ int *peso_plano, *peso_cenario;
 
 // up(i) --> Funcao que da o vertice na posicao a cima do vertice i.
 int up(int i) {
-  int res = i-n_colunas;
-  return (res < 0 ? -1 : res);
+  int res = i - n_colunas;
+  return (res <= 0 ? -1 : res);
 }
 
 // down(i) --> Funcao que da o vertice na posicao a baixo do vertice i.
 int down(int i) {
   int res = i + n_colunas;
-  return (res >= (n_linhas * n_colunas) ? -1 : res);
+  return (res > (n_vertices-1) ? -1 : res);
 }
 
 // left(i) --> Funcao que da o vertice na posicao esquerda do vertice i.
-int left(int i)  {
-  return (((i % n_colunas) == 0) ? -1 : i-1);
-}
+int left(int i)  { return (((i % n_colunas) == 1) ? -1 : i-1); }
 
 // right(i) --> Funcao que da o vertice na posicao direita do vertice i.
-int right(int i) {
-  return (((i % n_colunas) == (n_colunas-1)) ? -1 : i+1);
+int right(int i) { return (((i % n_colunas) == 0) ? -1 : i+1); }
+
+// encontraIndViz(i) --> Funcao que decide a que vizinho corresponde i.
+int encontraIndViz(int i) {
+  switch (i) {
+    case 0: return P;
+    case 1: return up(i);
+    case 2: return right(i);
+    case 3: return down(i);
+    case 4: return left(i);
+  }
+  return -1; // E suposto nunca acontecer.
 }
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 
+// - - - - - Funcoes para controlo da lista de vertices descobertos - - - - -
+
+// enqueque(int i) --> guarda o vertice i na lista.
+void enqueque(int i) {
+  No *novo = (No*) malloc(sizeof(No));
+
+  novo->id = i;
+  novo->next = NULL;
+
+  if (lista_incio == NULL) {
+    lista_incio = novo;
+    lista_fim   = novo;
+  } else {
+    lista_fim->next = novo;
+    lista_fim = novo;
+  }
+}
+
+// dequeque() --> retira o primeiro elemento da lista e devolve o seu id
+int dequeque() {
+  No *temp = lista_incio;
+  int res  = lista_incio->id;
+
+  lista_incio = lista_incio->next;
+  free(temp);
+  return res;
+}
+
+// freeLista() --> Liberta a memoria reservada para a lista;
+void freeLista() {
+  if (lista_incio != NULL) {
+    No *temp = lista_incio->next;
+    free(lista_incio);
+    lista_incio = temp;
+    freeLista();
+  }
+}
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
+
 // - - - - - - - - - - - - - Funcoes sobre o fluxo - - - - - - - - - - - - -
+
+// BFS() --> Encontra o caminho mais curto entre os vertices P e C.
+int BFS() {
+  int i, u, v, n_vizinhos;
+
+  for (i = 1; i < n_vertices; i++) {
+    cor[i]  = BRANCO;
+    dist[i] = -1;
+    pai[i]  = -1;
+  }
+  cor[P]  = CINZA;
+  dist[P] = 0;
+  pai[P]  = -2;
+
+  lista_incio = NULL;
+  lista_fim   = NULL;
+  enqueque(P);
+
+  while (lista_incio != NULL) {
+    u = dequeque();
+    n_vizinhos = (u == P ? n_vertices-1 : 6);
+
+    for (i = 0; i < n_vizinhos; i++) {
+      if (grafo_cap[u][i] != 0) { // As ligacoes com capacidade 0 nao contam.
+        if (u != P) {
+          if (i == C) {
+            freeLista();
+            return u; // Chegou-se ao target (C).
+          }
+
+          v = encontraIndViz(i);
+        } else { v = i; }
+
+        if (cor[v] == BRANCO) {
+          cor[v]  = CINZA;
+          dist[v] = dist[u] + 1;
+          pai[v]  = u;
+          enqueque(v);
+        }
+      }
+    }
+
+    cor[u] = PRETO;
+  }
+
+  return -1;  // ja nao e possivel chegar de P a C --> ja nao ha caminhos de aumento.
+}
+
+// edmonds_Karp() --> Calcula a divisao entre pontos de cenario e de primeiro plano a partir de fluxos.
+void edmonds_Karp() {
+  int paiTarget;
+
+  cor  = (int*) malloc(n_vertices * sizeof(int));
+  dist = (int*) malloc(n_vertices * sizeof(int));
+  pai  = (int*) malloc(n_vertices * sizeof(int));
+
+
+  for (paiTarget = BFS(); paiTarget != -1; paiTarget = BFS()) {
+    printf("BFS: %d\n", paiTarget);
+  }
+  free(cor);
+  free(dist);
+  free(pai);
+}
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 // ----------------------------------------------------------------------------
@@ -70,7 +194,8 @@ int right(int i) {
 
 
 int main(int argc, char const *argv[]) {
-  int i, j, temp;
+  int i, j;
+  int temp;
 
   scanf("%d %d\n\n", &n_linhas, &n_colunas);
   if (n_linhas < 1 || n_colunas < 1) {
@@ -79,32 +204,36 @@ int main(int argc, char const *argv[]) {
   }
 
   // Cria a matriz onde estarao as capacidades das ligacoes.
-  grafo_cap = (int**) malloc((n_linhas*n_colunas)*sizeof(int*));
-  for (i = 0; i < (n_linhas*n_colunas); i++) {
+  n_vertices = (n_linhas * n_colunas) + 1;
+  grafo_cap  = (int**) malloc(n_vertices * sizeof(int*));
+
+  grafo_cap[P] = (int*) malloc((n_vertices-1)*sizeof(int));
+  for (i = 1; i < n_vertices; i++) {
     grafo_cap[i] = (int*) malloc(6*sizeof(int));
     for (j = 0; j < 6; j++) {
       grafo_cap[i][j] = 0;
     }
   }
 
-  peso_plano   = (int*) malloc(n_linhas*n_colunas*sizeof(int));
-  peso_cenario = (int*) malloc(n_colunas*n_linhas*sizeof(int));
+  peso_plano   = (int*) malloc((n_vertices-1)*sizeof(int));
+  peso_cenario = (int*) malloc((n_vertices-1)*sizeof(int));
 
 
   // Le e guarda os pesos de 1 plano --> capacidades de ligar ao vertice P.
-  for (i = 0; i < n_linhas*n_colunas; i++) {
-    scanf("%d", &grafo_cap[i][P]);
-    peso_plano[i] = grafo_cap[i][P];
+  for (i = 0; i < (n_vertices-1); i++) {
+    scanf("%d", &grafo_cap[P][i]);
+    grafo_cap[i+1][P] = 0;
+    peso_plano[i] = grafo_cap[P][i];
   }
 
   // Le e guarda os pesos de cenario --> capacidades de ligar ao vertice C.
-  for (i = 0; i < n_linhas*n_colunas; i++) {
+  for (i = 1; i < n_vertices; i++) {
     scanf("%d", &grafo_cap[i][C]);
-    peso_cenario[i] = grafo_cap[i][C];
+    peso_cenario[i-1] = grafo_cap[i][C];
   }
 
   // Le e guarda as capacidades das ligacoes horizontais.
-  for (i = 0; i < n_linhas*n_colunas; i++) {
+  for (i = 1; i < n_vertices; i++) {
     j = right(i);
     if (j == -1) {
       grafo_cap[i][DIR] = 0;
@@ -115,7 +244,7 @@ int main(int argc, char const *argv[]) {
   }
 
   // Le e guarda as capacidades das ligacoes verticais.
-  for (i = 0; i < n_linhas*n_colunas; i++) {
+  for (i = 1; i < n_vertices; i++) {
     j = down(i);
     if (j == -1) {
       grafo_cap[i][BAIXO] = 0;
@@ -126,13 +255,17 @@ int main(int argc, char const *argv[]) {
   }
 
 
+  //Calcula a divisao entre pontos de cenario e de primeiro plano a partir de fluxos.
+  edmonds_Karp();
+
+
   // - - - - - - - Imprimir o input - - - - - - -
-  printf("%d %d\n\n", n_linhas, n_colunas);
+  printf("%d %d\nn_vertices: %d\n\n", n_linhas, n_colunas, n_vertices);
 
   printf("Pesos de plano:\n");
   temp = 0;
-  for (i = 0; i < n_linhas*n_colunas; i++) {
-    printf("%d ", grafo_cap[i][P]);
+  for (i = 0; i < (n_vertices-1); i++) {
+    printf("%d ", grafo_cap[P][i]);
     temp++;
     if (temp == n_colunas) {
       printf("\n");
@@ -143,7 +276,7 @@ int main(int argc, char const *argv[]) {
 
   printf("Pesos de cenario\n");
   temp = 0;
-  for (i = 0; i < n_linhas*n_colunas; i++) {
+  for (i = 1; i < n_vertices; i++) {
     printf("%d ", grafo_cap[i][C]);
     temp++;
     if (temp == n_colunas) {
@@ -155,7 +288,7 @@ int main(int argc, char const *argv[]) {
 
   printf("Ligacoes Para a direita (a ultima coluna e suposto ser 0):\n");
   temp = 0;
-  for (i = 0; i < n_linhas*n_colunas; i++) {
+  for (i = 1; i < n_vertices; i++) {
     printf("%d ", grafo_cap[i][DIR]);
     temp++;
     if (temp == n_colunas) {
@@ -167,7 +300,7 @@ int main(int argc, char const *argv[]) {
 
   printf("Ligacoes Para a esquerda (a primeira coluna e suposto ser 0):\n");
   temp = 0;
-  for (i = 0; i < n_linhas*n_colunas; i++) {
+  for (i = 1; i < n_vertices; i++) {
     printf("%d ", grafo_cap[i][ESQ]);
     temp++;
     if (temp == n_colunas) {
@@ -179,7 +312,7 @@ int main(int argc, char const *argv[]) {
 
   printf("Ligacoes para baixo (a ultima linha e suposto ser 0):\n");
   temp = 0;
-  for (i = 0; i < n_linhas*n_colunas; i++) {
+  for (i = 1; i < n_vertices; i++) {
     printf("%d ", grafo_cap[i][BAIXO]);
     temp++;
     if (temp == n_colunas) {
@@ -191,7 +324,7 @@ int main(int argc, char const *argv[]) {
 
   printf("Ligacoes para cima (a primeira linha e suposto ser 0):\n");
   temp = 0;
-  for (i = 0; i < n_linhas*n_colunas; i++) {
+  for (i = 1; i < (n_linhas*n_colunas)+1; i++) {
     printf("%d ", grafo_cap[i][CIMA]);
     temp++;
     if (temp == n_colunas) {
@@ -200,6 +333,15 @@ int main(int argc, char const *argv[]) {
     }
   }
   printf("\n");
+
+
+  for (i = 0; i < n_vertices; i++) {
+    free(grafo_cap[i]);
+  }
+  free(grafo_cap);
+
+  free(peso_plano);
+  free(peso_cenario);
 
   return 0;
 }
